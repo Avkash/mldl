7d03770ea70f3cd8ab6fae14c8720c9c93d3e195
Avkash Chauhan <avkash@gmail.com>
2017-11-20 14:41:43 -0800

    Added scala kmeans sample

---
+++ code/scala/H2O-KMeans-DriverSpeed.scala
@@ -0,0 +1,72 @@
+//### Importing key libraries:
+import org.apache.spark.h2o._
+import org.apache.spark.SparkFiles
+import org.apache.spark.examples.h2o._
+import org.apache.spark.sql.{DataFrame, SQLContext}
+import water.Key
+import water.support.SparkContextSupport.addFiles
+import water.support.H2OFrameSupport._
+import java.io.File
+
+
+//### Creating H2O Context:
+val h2oContext = H2OContext.getOrCreate(sc)
+import h2oContext._
+import h2oContext.implicits._
+
+
+//### Importing titanic data from a file stored on local file system:
+val driversData = new H2OFrame(new File("/Users/avkashchauhan/learn/customers/driver_delivery_speed.csv"))
+
+//### Importing Kmeans specific libraries:
+import _root_.hex.kmeans
+import _root_.hex.kmeans.KMeans
+import _root_.hex.kmeans.KMeansModel
+import _root_.hex.kmeans.KMeansModel.KMeansParameters
+
+//### Creating Kmeans Model with fixed cluster size as given with _k parameter:
+val kmParams = new KMeansParameters()
+kmParams._train = driversData
+kmParams._k = 4
+kmParams._seed = 1234
+kmParams._max_iterations = 10
+kmParams._estimate_k = false
+kmParams._standardize = false;
+
+//### Creating Deep Learning Model with supplied key name
+val km = new KMeans(kmParams)
+val kmModel = km.trainModel.get()
+
+
+//### Creating Kmeans Model with estimate_k as true with max limit of cluster size as _k (100):
+kmParams._k = 100
+kmParams._estimate_k = true
+//###: Note: You must build model again after updating the above settings 
+
+//### Getting Model Details built in previous step:
+kmModel
+
+//### Getting Model summary and training metrics 
+kmModel._output._model_summary
+kmModel._output._training_metrics
+
+//###: Distributuon of all data into speicific clusters
+kmModel._output._size
+
+//###: Getting the size of the cluster
+kmModel._output._size.length
+
+//### Getting Model Metrics
+kmModel._output._k
+kmModel._output._k.length
+kmModel._output._centers_raw
+kmModel._output._centers_std_raw
+
+//### Getting Model features and classes
+kmModel._output.nfeatures
+kmModel._output.nclasses
+
+//### Getting Model classes, domains and checking if it is supervised or not
+kmModel._output.isSupervised
+kmModel._output.classNames
+kmModel._output._domains
